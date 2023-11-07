efb047555560841873256d31b8ce8c1482fa2acd
Epicsociety <measkswho@gmail.com>
2023-11-07 22:14:53 +0300

    Base64 part in the header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Authentication """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa: E501
+        """ Return the Base64 part of header """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        token = authorization_header.split(" ")[-1]
+        return token
