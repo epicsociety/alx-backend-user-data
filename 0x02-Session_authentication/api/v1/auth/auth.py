71cc5f7e2c45cd3392b937ff41d1df3dce7a6b47
Epicsociety <measkswho@gmail.com>
2023-11-10 02:35:58 +0300

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Class auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication system """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Manage paths"""
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path.rstrip('/')):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Handle API authetication """
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        return header if header else None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Flask request object """
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+""" Basic Auth module
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
+import base64
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication """
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:  # noqa: E501
+        """ Return the Base64 part of header """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        token = authorization_header.split(" ")[-1]
+        return token
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:  # noqa: E501
+        """ Returns the decoded value of a Base64 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded.decode('utf-8')
+            return decoded_str
+        except Exception:
+            return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str
+    ) -> (str, str):  # noqa: E501
+        """returns user email and password from the Base64 decoded value """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" in decoded_base64_authorization_header:
+            result = decoded_base64_authorization_header.split(":", 1)
+            return (result[0], result[1])
+        return (None, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ get the current user"""
+
+        Auth_header = self.authorization_header(request)
+        if Auth_header is not None:
+            token = self.extract_base64_authorization_header(Auth_header)
+            if token is not None:
+                decoded = self.decode_base64_authorization_header(token)
+                if decoded is not None:
+                    email, password = self.extract_user_credentials(decoded)
+                    if email is not None:
+                        return self.user_object_from_credentials(
+                            email, password)
+        return
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        returns the User instance based on his email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+            if not users or users == []:
+                return None
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
+            return None
+        except Exception:
+            return None
