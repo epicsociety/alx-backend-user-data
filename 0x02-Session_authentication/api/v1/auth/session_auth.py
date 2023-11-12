3e7932b175ef8348d4ee0968acff660cbdb66cf8
Epicsociety <measkswho@gmail.com>
2023-11-12 05:09:13 +0300

    Create a session id for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,19 @@
 """ Session auth module
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """A new authentication mechanism """
-    pass
+    def __init__(self):
+        """ Create an empty dict"""
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a Session ID for a user_id """
+        if user_id and isinstance(user_id, str):
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
