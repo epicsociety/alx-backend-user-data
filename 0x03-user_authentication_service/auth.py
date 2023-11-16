de9b753fa78df6d31e19fd9f02c81ac010c56365
Epicsociety <measkswho@gmail.com>
2023-11-16 14:34:50 +0300

    deletes a session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,8 +74,6 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """ deletes a session"""
-        if user_id is None:
-            return None
         try:
             user = self._db.find_user_by(id=user_id)
             self._db.update_user(user.id, session_id=None)
