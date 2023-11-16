5245ea71bd1cfd1a8d2871242d80bc9c304e3e94
Epicsociety <measkswho@gmail.com>
2023-11-16 20:37:19 +0300

    update password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,6 +2,7 @@
 """Flask app
 """
 from contextlib import redirect_stderr
+from turtle import TurtleGraphicsError
 from urllib import response
 from auth import Auth
 from flask import Flask, jsonify, request, abort, redirect, url_for
@@ -75,6 +76,20 @@ def get_reset_password_token() -> tuple:
         abort(403)
     reset_token = AUTH.get_reset_password_token(email)
     return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"}), 200
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def reset_password() -> tuple:
+    """ password update"""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        response_data = {"email": email, "message": "Password updated"}
+        return jsonify(response_data), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
