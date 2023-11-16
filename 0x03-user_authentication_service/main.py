492243f8499f1269acd29eb770e4ac5285c00690
Epicsociety <measkswho@gmail.com>
2023-11-16 21:10:42 +0300

    undo changes

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A simple end-to-end (E2E) integration test for `app.py`.
+"""
+import requests
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+BASE_URL = "http://0.0.0.0:5000"
