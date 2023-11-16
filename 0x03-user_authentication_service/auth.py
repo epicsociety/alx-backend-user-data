22f6ac214fc40c94d52102311a17f029ad1c6aa4
Epicsociety <measkswho@gmail.com>
2023-11-16 19:45:36 +0300

    reset_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,13 @@ class Auth:
         user = self._db.find_user_by(id=user_id)
         self._db.update_user(user.id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Reset the reset_token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
