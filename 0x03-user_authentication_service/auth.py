113e9d8fa3b0a62e4764277e15552241a0366b6a
Epicsociety <measkswho@gmail.com>
2023-11-16 20:17:07 +0300

    reset user's password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,13 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ reset user's password"""
+        user = self._db.find_user_by(reset_token=reset_token)
+        if not user:
+            raise ValueError
+        else:
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(user.id, reset_token=None)
